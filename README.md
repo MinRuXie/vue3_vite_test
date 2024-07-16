0e039ab099482cadc10f458bb8e510801957e604
joyxie <joyxie@labspace.com.tw>
2024-07-16 16:53:32 +0800

    update

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # Vue3 Vite 串 DummyJSON 練習
 * https://dummyjson.com/
 
+## 參考
+* [Vue 3 JWT Refresh Token with Axios example](https://github.com/bezkoder/vue-3-jwt-refresh-token)
 
 ## 頁面
 * 會員中心
-    * 登入 & 登出 & 權限頁面控制 (工時預估5天)
+    * 登入 & 登出 & 路由守衛 (工時預估5天)
         1. 登入：輸入 "帳號" & "密碼" 取得 "token" & "refreshToken"，並存在 sessionStorage。
-        2. 登入逾時："token" 過期，使用 "refreshToken" 去換一組新的 "token" & "refreshToken"。
-        3. 登出：清除 sessionStorage 的資料。
-        4. 權限頁面控制：未登入時不能瀏覽頁面。
+        2. 登出：清除 sessionStorage 的資料，跳轉登入頁。
+        3. 路由守衛：
+            1. 未登入時：跳轉登入頁。
+            2. "token" 過期：使用 "refreshToken" 去換一組新的 "token" & "refreshToken"。
+            3. "refreshToken"過期：跳轉登入頁。
     * 會員中心主頁
         * 列出使用者資訊
         * 修改資訊
--- src/components/admin/member-center/user/UserInfo.vue
+++ src/components/admin/member-center/user/UserInfo.vue
@@ -1,16 +1,25 @@
 <template>
 
-    <template v-if="!userData">
+    <template v-if="!data.userData">
         載入中...
     </template>
-    <template v-if="userData">
-        <img :src="userData.image" alt="avator">
+    <template v-if="data.userData">
+        <img :src="data.userData.image" alt="avator">
+        <h4>帳戶資訊</h4>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">username: {{ userData.username }}</li>
-            <li class="list-group-item">password: {{ userData.password }}</li>
-            <li class="list-group-item">name: {{ userData.firstName }} {{ userData.lastName }}</li>
-            <li class="list-group-item">gender: {{ userData.gender }}</li>
+            <li class="list-group-item">username: {{ data.userData.username }}</li>
+            <li class="list-group-item">password: {{ data.userData.password }}</li>
+            <li class="list-group-item">name: {{ data.userData.firstName }} {{ data.userData.lastName }}</li>
+            <li class="list-group-item">gender: {{ data.userData.gender }}</li>
         </ul>
+
+        <h4>文章</h4><span>共{{ data.postsData.total }}篇</span>
+        <ul class="list-group list-group-flush">
+            <li v-for="(item, index) of data.postsData.posts" :key="index" class="list-group-item">
+                {{ item.title }} --- 瀏覽:{{ item.views }}次 / 喜歡:{{ item.reactions.likes }}人 / 不喜歡: {{ item.reactions.dislikes }}人
+            </li>
+        </ul>
+
     </template>
 
     
@@ -20,43 +29,89 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, onMounted } from 'vue';
 
 export default {
     name: 'UserInfo',
     setup() {
         // 資料
-        let userData = ref(null); // API資料是非同步回傳, 必須用響應式接住
 
+        // Vue 3 - Fetch Data from an API
+        // https://jasonwatmore.com/vue-3-fetch-data-from-an-api
+        // let userData = ref(null); // API資料是非同步回傳, 必須用響應式接住
+        // let userData = reactive({
+        //     data: null
+        // });
 
-        // 使用 token 取得登入者資訊
-        /* providing token in bearer */
-        fetch('https://dummyjson.com/auth/me', {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${sessionStorage.getItem("token")}`, 
-            }, 
+        let data = reactive({
+            userData: null,
+            postsData: null,
         })
-        .then(res => {
-            // console.log(res.status); // Will show you the status
-            if (!res.ok) {
-                throw new Error("HTTP status " + res.status);
-            }
-            return res.json();
+
+
+
+
+        async function getData() {
+            // 使用 token 取得登入者資訊
+            /* providing token in bearer */
+            await fetch('https://dummyjson.com/auth/me', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${sessionStorage.getItem("token")}`, 
+                }, 
+            })
+            .then(res => {
+                // console.log(res.status); // Will show you the status
+                if (!res.ok) {
+                    throw new Error("HTTP status " + res.status);
+                }
+                return res.json();
+            })
+            .then(json => {
+                // console.log(json);
+
+                // userData.value = structuredClone(json);
+                // console.log('userData', userData);
+
+                data.userData = structuredClone(json);
+                // console.log('data.userData', data.userData);
+            });
+
+
+
+            // if ( data.userData ) {
+                // console.log('data.userData', data.userData);
+                // console.log('data.userData.id', data.userData.id);
+            // }
+
+            // 使用 id 取得文章列表
+            /* getting posts of user with id 5 */
+            fetch(`https://dummyjson.com/users/${data.userData.id}/posts`)
+            .then(res => res.json())
+            .then(json => {
+                console.log('json', json);
+
+                data.postsData = structuredClone(json);
+            });
+        }
+
+
+        onMounted(()=>{
+            // init
+            getData();
         })
-        .then(json => {
-            // console.log(json);
 
-            userData.value = structuredClone(json);
-            // console.log('userData', userData);
-        });
 
-        // console.log('userData', userData);
+        
+        
+
+        
+
 
 
 
         return {
-            userData
+            data
         }
     }
 }
